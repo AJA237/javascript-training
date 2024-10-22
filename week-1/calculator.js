22150e3f0eea74d2e10ecf8a2e13676709efcbe5
jordan_agha <azumbij@gmail.com>
2024-10-22 11:18:28 +0100

    feat: implementation of basic calculator function

---
+++ week-1/calculator.js
@@ -0,0 +1,12 @@
+function calculateBasic(num1, num2, operator) {
+    if (operator === "+") {
+        return num1 + num2;
+    } else if (operator === "-") {
+        return num1 - num2;
+    } else if (operator === "*") {
+        return num1 * num2;
+    } else if (operator === "/") {
+        return num1 / num2;
+    }
+}
+
